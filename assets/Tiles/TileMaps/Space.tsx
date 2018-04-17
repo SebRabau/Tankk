4944c6e833d36b61aa9ec24a050337f90925d78f
lfersan <leandrosantos@hotmail.co.uk>
2018-04-17 12:22:51 +0100

    Add files via upload

---
+++ assets/Tiles/TileMaps/Space.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Space" tilewidth="32" tileheight="32" tilecount="440" columns="20">
+ <image source="../TileAssets/space-tileset/space-blks-1.034.png" width="640" height="704"/>
+</tileset>
