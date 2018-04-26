f9947ab2aa8a1217dd6cef40747bfcff8ef86b05
lfersan <leandrosantos@hotmail.co.uk>
2018-04-26 11:43:08 +0100

    Add files via upload

---
+++ assets/Tiles/TileMaps/Smoke.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sprites" tilewidth="32" tileheight="32" tilecount="112" columns="4">
+ <image source="C:/Users/lafs10/Downloads/sprites.png" width="128" height="896"/>
+</tileset>
---
+++ assets/Tiles/TileMaps/Tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset2" tilewidth="32" tileheight="32" tilecount="1440" columns="45">
+ <image source="../TileAssets/WarZone Assets/tds-modern-tilesets-environment/PNG/Source/Tileset.png" width="1440" height="1024"/>
+</tileset>
