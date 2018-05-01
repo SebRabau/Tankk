892c82519ebc3d6ed3c23f1848ad2f30f9655c79
lfersan <leandrosantos@hotmail.co.uk>
2018-05-01 16:42:29 +0100

    Add files via upload

--- assets/Tiles/TileMaps/Base.tsx
+++ assets/Tiles/TileMaps/Base.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Base" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+<tileset name="Base" tilewidth="32" tileheight="32" tilecount="16" columns="4">
  <grid orientation="orthogonal" width="263" height="139"/>
- <image source="../TileAssets/WarZone Assets/tds-modern-tilesets-environment/PNG/House/TDS04_House02.png" width="263" height="139"/>
+ <image source="../TileAssets/WarZone Assets/tds-modern-tilesets-environment/PNG/House/TDS04_0000_House01.png" width="132" height="132"/>
 </tileset>
--- assets/Tiles/TileMaps/Tree_01.tsx
+++ assets/Tiles/TileMaps/Tree_01.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tree_01" tilewidth="140" tileheight="131" tilecount="16" columns="1">
+<tileset name="Tree_01" tilewidth="35" tileheight="32" tilecount="16" columns="4">
  <grid orientation="orthogonal" width="32" height="32"/>
  <image source="../TileAssets/WarZone Assets/tds-modern-tilesets-environment/PNG/Trees Bushes/TDS04_0019_Tree4.png" width="140" height="131"/>
 </tileset>
