e4d1e1215165bd4b12a6680c594a62afa5b1ddb8
lfersan <leandrosantos@hotmail.co.uk>
2018-05-01 15:51:00 +0100

    Add files via upload

--- assets/Tiles/TileMaps/Humvee_Broken_05.tsx
+++ assets/Tiles/TileMaps/Humvee_Broken_05.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Humvee_Broken_05" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+<tileset name="Humvee_Broken_05" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <grid orientation="orthogonal" width="128" height="128"/>
  <image source="../TileAssets/WarZone Assets/tds-pixel-art-modern-soldiers-and-vehicles-sprites/Humvee/Broken/Humvee_Broken_0005.png" width="128" height="128"/>
 </tileset>
