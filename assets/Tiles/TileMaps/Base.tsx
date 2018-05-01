7800a865dea8a94c1b28427f24aeb6b2be1614c0
lfersan <leandrosantos@hotmail.co.uk>
2018-05-01 14:28:11 +0100

    Add files via upload

--- assets/Tiles/TileMaps/Base.tsx
+++ assets/Tiles/TileMaps/Base.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Base" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+<tileset name="Base" tilewidth="64" tileheight="64" tilecount="9039" columns="4">
+ <grid orientation="orthogonal" width="32" height="32"/>
  <image source="../TileAssets/WarZone Assets/tds-modern-tilesets-environment/PNG/House/TDS04_House02.png" width="263" height="139"/>
 </tileset>
