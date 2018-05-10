bb520808bc3275b9db40bd0adfde89b54f5532af
joshruane98 <joshuaruane@gmail.com>
2018-05-10 16:51:51 +0100

    Updated Lose

--- js/Lose.js
+++ js/Lose.js
@@ -4,8 +4,9 @@ Tankk.Lose.prototype = {
     create: function() {
         this.backgroundColor = "#000"; 
         
-        var splash = this.add.sprite(window.innerWidth/2, window.innerHeight/2 - 200, "lose"); //new lose screen required
+        var splash = this.add.sprite(window.innerWidth/2, window.innerHeight/2 - 200, "logo"); //new lose screen required
         splash.anchor.setTo(0.5, 0.5);
+        splash.scale.setTo(0.25, 0.25);
         
         var loseText = this.add.text(window.innerWidth/2, window.innerHeight/2, "Your final score: "+score, {
             font: "30px Arial",
@@ -28,11 +29,18 @@ Tankk.Lose.prototype = {
         }, this, 1, 0, 2, 0);
         restartButton.anchor.setTo(0.5);*/
         
-        var restartButton = this.add.button(window.innerWidth/2, window.innerHeight/2 + 200, "playButton", function() {
+        var homeButton = this.add.button(window.innerWidth/2 - 100, window.innerHeight/2 + 200, "home", function() {
             this.game.sound.stopAll();
             this.state.start("MainMenu", true, false, score);            
         }, this, 1, 0, 2, 0);
-        restartButton.anchor.setTo(0.5);
-        restartButton.scale.setTo(0.07);
+        homeButton.anchor.setTo(0.3);
+        homeButton.scale.setTo(0.3);
+        
+        var redoButton = this.add.button(window.innerWidth/2 + 50, window.innerHeight/2 + 200, "redo", function() {
+            this.game.sound.stopAll();
+            this.state.start("MainMenu", true, false, score);            
+        }, this, 1, 0, 2, 0);
+        redoButton.anchor.setTo(0.3);
+        redoButton.scale.setTo(0.3);
     }
 }
